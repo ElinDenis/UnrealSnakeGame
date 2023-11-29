f9329ba0f35b0e255ec02976148e77c238c2e457
Elin Denis <el1n.denis@yandex.ru>
2023-11-29 11:31:36 +0500

    Lesson 1 - Tests

---
+++ Source/SnakeGame/Tests/World.spec.cpp
@@ -0,0 +1,47 @@
+// SnakeGame. Copyright Elin Denis. All Rights Reserved.
+// Developed with Unreal Engine 5.3
+#if WITH_AUTOMATION_TESTS
+
+#include "CoreMinimal.h"
+#include "Misc/AutomationTest.h"
+
+// Engine class include
+#include "Kismet/GameplayStatics.h"
+
+// Project class include
+#include "Tests/Utils/TestUtils.h"
+#include "World/SG_Grid.h"
+
+BEGIN_DEFINE_SPEC(FSnakeWorld, "Category",
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority)
+
+UWorld* World;
+
+END_DEFINE_SPEC(FSnakeWorld)
+
+void FSnakeWorld::Define()
+{
+    using namespace Snake::Test;
+
+    Describe("WorldGrid",
+        [this]()
+        {
+            BeforeEach(
+                [this]()
+                {
+                    AutomationOpenMap("GameLevel");
+                    World = GetTestGameWorld();
+                });
+
+            It("OnlyOneValidGridActorShouldExists",
+                [this]()
+                {
+                    TArray<AActor*> Grids;
+                    UGameplayStatics::GetAllActorsOfClass(World, ASG_Grid::StaticClass(), Grids);
+                    TestTrueExpr(Grids.Num() == 1);
+                    TestNotNull("Grid actro exists", Grids[0]);
+                });
+        });
+}
+
+#endif
